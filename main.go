8d6c73e848f8260da621f68fbc94ea8de8cbb034
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2021-08-12 15:46:16 +0300

    Solutions for LeetCode Problem: https://leetcode.com/problems/utf-8-validation/submissions/

--- main.go
+++ main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
-	"algo/numbers"
 	"fmt"
-	"math"
 )
 
-func main() {
-	result := numbers.TrapezoidCalculate(0.0, 5.0, 10, func(x float64) float64 {
-		return 1 + x + math.Sin(2*x)
-	})
+func validUtf8(data []int) bool {
+	waiting := 0
+	for _, byte := range data {
+		str := fmt.Sprintf("%08b", byte)
 
-	fmt.Println(result)
+		if waiting > 0 {
+			if str[0] == '0' {
+				return false
+			} else if str[:2] == "10" {
+				waiting--
+			} else if str[:2] == "11" {
+				return false
+			} else {
+				return false
+			}
+		} else if waiting <= 0 {
+			if str[:2] == "10" {
+				return false
+			} else if str[:3] == "110" {
+				waiting = 1
+			} else if str[:4] == "1110" {
+				waiting = 2
+			} else if str[:5] == "11110" {
+				waiting = 3
+			} else if str[:1] == "0" {
+				continue
+			} else {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+
+	return waiting <= 0
+}
+
+func main() {
+	result := validUtf8([]int{237})
+
+	fmt.Printf("Result: %t Expected: %t ", result, false)
 }
